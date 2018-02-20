eabcdda719a261a1543f7f8e22209340998431bd
PRESTONPOWELL22 <preston@windowtorecovery.com>
2018-02-19 16:32:33 -0800

    did some stuff

---
+++ assets/javascript/app.js
@@ -0,0 +1,163 @@
+// button is #game
+// row for game is #gameRow
+
+$(document).ready(function(){
+    $(".hiddenContent").hide()
+
+
+    // global vars
+    var questions = [
+        "1. What is the set of markup symbols or codes inserted in a file intended for display on a World Wide Web browser page called?",
+        "2. What describes how HTML elements are to be displayed/styled?",
+        "3. A javascript library that helps with dynamic page manipulation",
+        "4. What is the name of the popular css library made by twitter",
+        "5. What is the name of javascript framework that facebook designed?",
+        "6. ",
+        "7. ",
+        "8. ",
+        "9. ",
+        "10. "
+
+    ]
+
+    var answers = [
+        "Hypertext Markup Language",
+        "Cascading Style Sheets",
+        "jQuery",
+        "Bootstrap",
+        "React.js",
+        "",
+        "",
+        "",
+        "",
+        ""        
+    ]
+
+    var correct = 0
+    var incorrect = 0
+    var sec = 60
+    
+    
+    
+    
+
+
+
+    // function that creates the game 
+    $("#game").click(function(){
+        $(".jumbotron").hide()
+        $('.hiddenContent').show()
+
+        var sec = 100
+    
+    
+    
+    
+        var timer = setInterval(function() {
+        $('.timer').text(sec--);
+          if (sec == -1) {
+            $('.timer .countdown').fadeOut('slow');
+            clearInterval(timer);
+          }
+          if (sec === 0){
+            alert("youre out of time") 
+          }  
+        }, 1000);
+ 
+        // question1
+        $(".q1").append(questions[0])
+        $(".q1a1").append(answers[0])
+        $(".q1a2").append(answers[1])
+        $(".q1a3").append(answers[4])
+
+        // question2
+        $(".q2").append(questions[1])
+        $(".q2a1").append(answers[0])
+        $(".q2a2").append(answers[2])
+        $(".q2a3").append(answers[1])
+
+        // question 3
+        $(".q3").append(questions[2])
+        $(".q3a1").append(answers[0])
+        $(".q3a2").append(answers[2])
+        $(".q3a3").append(answers[1])
+
+
+        // question 4
+        $(".q4").append(questions[3])
+        $(".q4a1").append(answers[1])
+        $(".q4a2").append(answers[7])
+        $(".q4a3").append(answers[3])
+
+        // question 5
+        $(".q5").append(questions[4])
+        $(".q5a1").append(answers[0])
+        $(".q5a2").append(answers[2])
+        $(".q5a3").append(answers[1])
+
+        // question 6
+        $(".q6").append(questions[5])
+        $(".q6a1").append(answers[0])
+        $(".q6a2").append(answers[2])
+        $(".q6a3").append(answers[1])
+
+        // question 7
+        $(".q7").append(questions[6])
+        $(".q7a1").append(answers[0])
+        $(".q7a2").append(answers[2])
+        $(".q7a3").append(answers[1])
+
+        // question 8
+        $(".q8").append(questions[7])
+        $(".q8a1").append(answers[0])
+        $(".q8a2").append(answers[2])
+        $(".q8a3").append(answers[1])
+
+        // question 9
+        $(".q9").append(questions[8])
+        $(".q9a1").append(answers[0])
+        $(".q9a2").append(answers[2])
+        $(".q9a3").append(answers[1])
+
+        // question 10
+        $(".q10").append(questions[9])
+        $(".q10a1").append(answers[0])
+        $(".q10a2").append(answers[2])
+        $(".q10a3").append(answers[1])
+
+
+
+
+
+                
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
