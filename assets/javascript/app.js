7a05fec0a35caf95d21a6d5ab9b3eedb5d61f4a5
PRESTONPOWELL22 <preston@windowtorecovery.com>
2018-02-21 15:27:10 -0800

    changed to a form

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,14 +9,14 @@ $(document).ready(function(){
     var questions = [
         "1. What is the set of markup symbols or codes inserted in a file intended for display on a World Wide Web browser page called?",
         "2. What describes how HTML elements are to be displayed/styled?",
-        "3. A javascript library that helps with dynamic page manipulation",
-        "4. What is the name of the popular css library made by twitter",
+        "3. What is the name of a javascript library that helps with dynamic page manipulation?",
+        "4. What is the name of the popular css library made by twitter?",
         "5. What is the name of javascript framework that facebook designed?",
-        "6. ",
-        "7. ",
-        "8. ",
-        "9. ",
-        "10. "
+        "6. jQuery is a _______ library?",
+        "7. This game sucks?",
+        "8. What is node.js?",
+        "9. What is Google's javascript framework called?",
+        "10. What does the duck say?"
 
     ]
 
@@ -26,11 +26,11 @@ $(document).ready(function(){
         "jQuery",
         "Bootstrap",
         "React.js",
-        "",
-        "",
-        "",
-        "",
-        ""        
+        "Javascript",
+        "True",
+        "A javascript runtime environment",
+        "Angular.js",
+        "Quack"        
     ]
 
     var correct = 0
@@ -39,11 +39,13 @@ $(document).ready(function(){
     
     
     
+
+    
     
 
 
 
-    // function that creates the game 
+    // function that creates the game once you click the start button
     $("#game").click(function(){
         $(".jumbotron").hide()
         $('.hiddenContent').show()
@@ -63,12 +65,12 @@ $(document).ready(function(){
             alert("youre out of time") 
           }  
         }, 1000);
- 
+        var num = Math.floor(Math.random * 10)
         // question1
         $(".q1").append(questions[0])
-        $(".q1a1").append(answers[0])
-        $(".q1a2").append(answers[1])
-        $(".q1a3").append(answers[4])
+        $(".q1a1").append(answers[3])
+        $(".q1a2").append(answers[7])
+        $(".q1a3").append(answers[0])
 
         // question2
         $(".q2").append(questions[1])
@@ -98,41 +100,44 @@ $(document).ready(function(){
         // question 6
         $(".q6").append(questions[5])
         $(".q6a1").append(answers[0])
-        $(".q6a2").append(answers[2])
+        $(".q6a2").append(answers[5])
         $(".q6a3").append(answers[1])
 
         // question 7
         $(".q7").append(questions[6])
-        $(".q7a1").append(answers[0])
-        $(".q7a2").append(answers[2])
-        $(".q7a3").append(answers[1])
+        $(".q7a1").append(answers[6])
+        $(".q7a2").append("false")
+        $(".q7a3").append("chicken noodle soup")
 
         // question 8
         $(".q8").append(questions[7])
         $(".q8a1").append(answers[0])
-        $(".q8a2").append(answers[2])
+        $(".q8a2").append(answers[7])
         $(".q8a3").append(answers[1])
 
         // question 9
         $(".q9").append(questions[8])
-        $(".q9a1").append(answers[0])
-        $(".q9a2").append(answers[2])
+        $(".q9a1").append(answers[4])
+        $(".q9a2").append(answers[8])
         $(".q9a3").append(answers[1])
 
         // question 10
         $(".q10").append(questions[9])
         $(".q10a1").append(answers[0])
-        $(".q10a2").append(answers[2])
+        $(".q10a2").append(answers[9])
         $(".q10a3").append(answers[1])
-
-
+        
+        
 
 
 
                 
     })
-
-
+    $(function() {
+        $('form').submit(function() {
+          console.log(value);
+        });
+      });
 
 
 
