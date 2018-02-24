5a04a760958871e323e830958e9ae3d5db1b40dd
PRESTONPOWELL22 <preston@windowtorecovery.com>
2018-02-24 01:04:58 -0800

    after a long fought battle, the code works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,6 +20,65 @@ $(document).ready(function(){
 
     ]
 
+    var one = {
+        question: "What is the set of markup symbols or codes inserted in a file intended for display on a World Wide Web browser page called?",
+        rightAns: 2,
+        answers: ["Chinese", "Javascript", "Hypertext Markup Language"]
+    }
+
+    var two = {
+        question: "What describes how HTML elements are to be displayed/styled?",
+        rightAns: 1,
+        answers: ["Microdata", "CSS", "Sass"]
+    }
+
+    var three = {
+        question: "What is the name of a javascript library that makes dynamic page manipulation easier?",
+        rightAns: 0,
+        answers: ["jQuery", "React.js", "Angular.js"]
+    }
+
+    var four = {
+        question: "What is the name of the popular css library made by twitter?",
+        rightAns: 2,
+        answers: ["Twitter Public Library", "Sass", "Bootstrap"]
+    }
+
+    var five = {
+        question: "jQuery is a _______ library?",
+        rightAns: 0,
+        answers:["Javascript","CSS","HTML"]
+    }
+
+    var six = {
+        question: "This game sucks?",
+        rightAns: 0,
+        answers:["Hotdog","True","False"]
+    }
+
+    var seven = {
+        question: "What is node.js?",
+        rightAns: 1,
+        answers:["A javascript library","A javascript runtime environment","Another name for bootstrap"]
+    }
+
+    var eight = {
+        question: "What is Google's javascript framework called?",
+        rightAns: 1,
+        answers:["Node.js","Angular.js","React.js"]
+    }
+
+    var nine = {
+        question: "What is the name of javascript framework that facebook designed?",
+        rightAns: 2,
+        answers:["Bootstrap","Angular.js","React.js"]
+    }
+
+    var ten = {
+        question: "What does the duck say?",
+        rightAns: 0,
+        answers:["Quack","Moo","Shux"]
+    }
     var answers = [
         "Hypertext Markup Language",
         "Cascading Style Sheets",
@@ -65,70 +124,91 @@ $(document).ready(function(){
             $(".timer").html("you ran outta time dawg") 
           }  
         }, 1000);
-        var num = Math.floor(Math.random * 10)
+        // var num = Math.floor(Math.random * 10)
         // question1
-        $(".q1").append(questions[0])
-        $(".q1a1").append(answers[3])
-        $(".q1a2").append(answers[7])
-        $(".q1a3").append(answers[0])
+        $(".q1").append(one.question)
+        $(".q1a1").append(one.answers[0])
+        $(".q1a2").append(one.answers[1])
+        $(".q1a3").append(one.answers[2])
 
         // question2
-        $(".q2").append(questions[1])
-        $(".q2a1").append(answers[0])
-        $(".q2a2").append(answers[2])
-        $(".q2a3").append(answers[1])
+        $(".q2").append(two.question)
+        $(".q2a1").append(two.answers[0])
+        $(".q2a2").append(two.answers[1])
+        $(".q2a3").append(two.answers[2])
 
         // question 3
-        $(".q3").append(questions[2])
-        $(".q3a1").append(answers[0])
-        $(".q3a2").append(answers[2])
-        $(".q3a3").append(answers[1])
+        $(".q3").append(three.question)
+        $(".q3a1").append(three.answers[0])
+        $(".q3a2").append(three.answers[1])
+        $(".q3a3").append(three.answers[2])
 
 
         // question 4
-        $(".q4").append(questions[3])
-        $(".q4a1").append(answers[1])
-        $(".q4a2").append(answers[7])
-        $(".q4a3").append(answers[3])
+        $(".q4").append(four.question)
+        $(".q4a1").append(four.answers[0])
+        $(".q4a2").append(four.answers[1])
+        $(".q4a3").append(four.answers[2])
 
         // question 5
-        $(".q5").append(questions[4])
-        $(".q5a1").append(answers[0])
-        $(".q5a2").append(answers[4])
-        $(".q5a3").append(answers[1])
+        $(".q5").append(five.question)
+        $(".q5a1").append(five.answers[0])
+        $(".q5a2").append(five.answers[1])
+        $(".q5a3").append(five.answers[2])
 
         // question 6
-        $(".q6").append(questions[5])
-        $(".q6a1").append(answers[0])
-        $(".q6a2").append(answers[5])
-        $(".q6a3").append(answers[1])
+        $(".q6").append(six.question)
+        $(".q6a1").append(six.answers[0])
+        $(".q6a2").append(six.answers[1])
+        $(".q6a3").append(six.answers[2])
 
         // question 7
-        $(".q7").append(questions[6])
-        $(".q7a1").append(answers[6])
-        $(".q7a2").append("false")
-        $(".q7a3").append("chicken noodle soup")
+        $(".q7").append(seven.question)
+        $(".q7a1").append(seven.answers[0])
+        $(".q7a2").append(seven.answers[1])
+        $(".q7a3").append(seven.answers[2])
 
         // question 8
-        $(".q8").append(questions[7])
-        $(".q8a1").append(answers[0])
-        $(".q8a2").append(answers[7])
-        $(".q8a3").append(answers[1])
+        $(".q8").append(eight.question)
+        $(".q8a1").append(eight.answers[0])
+        $(".q8a2").append(eight.answers[1])
+        $(".q8a3").append(eight.answers[2])
 
         // question 9
-        $(".q9").append(questions[8])
-        $(".q9a1").append(answers[4])
-        $(".q9a2").append(answers[8])
-        $(".q9a3").append(answers[1])
+        $(".q9").append(nine.question)
+        $(".q9a1").append(nine.answers[0])
+        $(".q9a2").append(nine.answers[1])
+        $(".q9a3").append(nine.answers[2])
 
         // question 10
-        $(".q10").append(questions[9])
-        $(".q10a1").append(answers[0])
-        $(".q10a2").append(answers[9])
-        $(".q10a3").append(answers[1])
+        $(".q10").append(ten.question)
+        $(".q10a1").append(ten.answers[0])
+        $(".q10a2").append(ten.answers[1])
+        $(".q10a3").append(ten.answers[2])
         
         
-
+        $( "form" ).submit(function( event ) {
+            event.preventDefault();
+           
+            var ans = $('select option:selected'); // [$(option) ... $(option)]
+            var valuess = []
+            $.map(ans, function(elem) {
+                valuess.push(parseInt($(elem).val()));
+              });
+            // console.log(valuess);
+            function getSum(total, num) {
+                return total + num;
+            }
+            correct = valuess.reduce(getSum)
+            // console.log(correct)
+            wrong = 10 - correct
+            $(".hiddenContent").hide()
+            $('#correct').append("you got " + correct + " right")
+            $('#wrong').append("you got " + wrong + " wrong")
+            percent = correct*10
+            $("#percent").append("you scored " + percent + "%")
+            
+        });
 
 
                 
